485b2f5745adcc14c2e9c2544d57b87e51328ad5
Julian <jorsini3565@gmail.com>
2022-02-25 23:18:36 -0400

    Slime Update
    Added more code to get Slime moving

--- Never Ending Game/Assets/Scripts/Enemys/Slime.cs
+++ Never Ending Game/Assets/Scripts/Enemys/Slime.cs
@@ -4,45 +4,56 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [Header("Private settings")]
     [HideInInspector]
     public bool mustPatrol;
+    private bool mustTurn;
 
+    [Header("Slime settings")]
     public Rigidbody2D rb;
     public float walkSpeed;
     public float range;
-    private float distToPlayer;
-    public Transform player;
+    public Transform groundCheckPos;
+    public LayerMask groundLayer;
     void Start()
     {
         mustPatrol = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (mustPatrol)
         {
             Patrol();
         }
+    }
 
-        distToPlayer = Vector2.Distance(transform.position, player.position);
-
-        if(distToPlayer <= range)
+    private void FixedUpdate()
+    {
+        if (mustPatrol)
         {
-            if(player.position.x > transform.position.x && transform.localScale.x < 0)
-            Shoot();
+            //Checks for ground
+            mustTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
         }
     }
 
     void Patrol()
     {
+        if (mustTurn)
+        {
+            Flip();
+        }
+        //moves slime
         rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
     }
 
     void Flip()
     {
+        //Moves back forth on platform, stop at edges of platform and turn around continuing to patrol
+        mustPatrol = false;
         transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
         walkSpeed *= -1;
+        mustPatrol = true;
     }
 
     void Shoot()
